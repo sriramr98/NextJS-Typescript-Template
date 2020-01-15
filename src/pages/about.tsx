27a970a60c0de99d437380bf730820d912275725
sriramr <sriramr083@gmail.com>
2020-01-15 19:14:36 +0530

    small fixes

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import Link from 'next/link';
 
 import Layout from 'components/layouts/AuthorizedLayout';
@@ -7,13 +9,11 @@ import Layout from 'components/layouts/AuthorizedLayout';
 const AboutPage: React.FunctionComponent = () => (
   <Layout title="About | Next.js + TypeScript Example">
     <Box>
-      <h1>About</h1>
-      <p>This is the about page</p>
-      <p>
-        <Link href="/">
-          <a>Go home</a>
-        </Link>
-      </p>
+      <Typography variant="h4">About</Typography>
+      <Typography variant="caption">This is the about page</Typography>
+      <Link href="/">
+        <Button>Go home</Button>
+      </Link>
     </Box>
   </Layout>
 );
